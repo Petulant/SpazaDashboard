24bd23f98f4154db44e4469efdf2dc67795810ba
Thembelihle <pmokhwebane@gmail.com>
2019-02-01 10:32:00 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,51 @@
+
+
+import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
+import { routesInfor } from './router.module';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule } from '@angular/material';
+
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { MyDashboardComponent} from './my-dashboard/my-dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TableComponent } from './table/table.component';
+import { ChartsComponent } from './charts/charts.component';
+import { MapComponent } from './map/map.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    LoginComponent,
+    MyDashboardComponent,
+    ProfileComponent,
+    TableComponent,
+    ChartsComponent,
+    MapComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule.forRoot(routesInfor)
+  ],
+  exports: [MatButtonModule, MatCheckboxModule, MatSidenavModule, MatListModule],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/router.module.ts
@@ -0,0 +1,27 @@
+
+import { Routes } from '@angular/router';
+
+
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { LoginComponent } from './login/login.component';
+import { MyDashboardComponent} from './my-dashboard/my-dashboard.component';
+import { ProfileComponent } from './profile/profile.component';
+import { TableComponent } from './table/table.component';
+import { ChartsComponent } from './charts/charts.component';
+import { MapComponent } from './map/map.component';
+
+
+
+
+const mypaths: Routes = [
+    {path: '', component: LoginComponent },
+    {path: 'sidebar', component: SidebarComponent, children: [
+        {path: 'my-dashboard', component: MyDashboardComponent},
+        {path: 'profile', component: ProfileComponent},
+        {path: 'table', component: TableComponent},
+        {path: 'charts', component: ChartsComponent},
+        {path: 'map', component: MapComponent}
+    ]},
+];
+
+export const routesInfor = mypaths ;
