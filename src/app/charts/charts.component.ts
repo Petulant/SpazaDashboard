03349342608b78cf87752cd77814f9f7567dd771
realSanele <therealsanele@gmail.com>
2019-02-15 14:20:05 +0200

    Chart fixed v2.0

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -14,18 +14,15 @@ export class ChartsComponent implements OnInit {
    responsive: true
  };
 
- public barChartsLabels: Array<string> = ['Soshanguve', 'PretoriaCBD', 'Mamelodi' ];
+ public barChartsLabels: Array<string> = ['Total Users', 'Total Spaza'];
 //  public barChartsLabels = [];
- public barChartsType = 'bar';
- public barChartsLegend = true;
- public barChartsData = [
-   {data: [23, 65 , 45], label: 'Series A'},
-   {data: [18, 14 , 10], label: 'Series B'}
- ];
+//  public barChartsType = 'bar';
+//  public barChartsLegend = true;
+ public barChartsData = [];
 
   public PieChartsLabels: Array<string> = [];
-  public PieChartsType = 'pie';
-  public PieChartsLegend = true;
+  // public PieChartsType = 'pie';
+  // public PieChartsLegend = true;
 //   public PieChartsData = [
 //    {data: [23, 65 , 45], label: 'Series A'},
 //    {data: [1, 1 , 1], label: 'Series A'},
@@ -44,6 +41,7 @@ export class ChartsComponent implements OnInit {
 
   async getAllTownships() {
     var mySpazasRef;
+    var totalSpaza = 0;
 
     var usersRef = firebase.database().ref("/users/").on("value", (snapshot) => {
 
@@ -60,6 +58,7 @@ export class ChartsComponent implements OnInit {
                 if(this.PieChartsLabels[i] === element.val().cityName){
                   console.log("City found")
                   this.PieChartsData[i]++;
+                  totalSpaza++;
                   isFound = true;
                   break;
                 }
@@ -69,39 +68,49 @@ export class ChartsComponent implements OnInit {
                 console.log("City not found")
                 var index = this.PieChartsLabels.push(element.val().cityName);
                 this.PieChartsData[index-1] = 1;
-
+                totalSpaza++;
               }
 
 
             }else{
               var index = this.PieChartsLabels.push(element.val().cityName);
               this.PieChartsData[index-1] = 1;
+              totalSpaza++;
               console.log(this.PieChartsLabels)
             }
 
+            // if(element != null){
+            //   console.log(Object.keys(element).length);
+            //   this.barChartsData[1] = this.barChartsData[1] + Object.keys(element).length; // --> PieChartsData[total_Users, total_Spaza]
+            // }
 
           });
+          console.log(snapshot);
+          
         });
       });
-      this.showPie()
+      this.barChartsData[1] = totalSpaza;
+      this.barChartsData[0] = Object.keys(snapshot).length; // --> PieChartsData[total_Users, total_Spaza]
+      this.showPie();
+      this.showAppUsers();
       console.log(this.PieChartsData)
       console.log(this.PieChartsLabels)
     });
   }
 
-  searchIfExist(cityName: string) {
-    var isFound: boolean = false;
+  // searchIfExist(cityName: string) {
+  //   var isFound: boolean = false;
 
-    if (this.barChartsLabels.length !== 0 && this.barChartsLabels != null) {
-      for (let i = 0; i <= this.barChartsLabels.length; i++) {
-        if (this.barChartsLabels[1].toLowerCase === cityName.toLowerCase) {
-          isFound = true;
-        }
-      }
-    }
+  //   if (this.barChartsLabels.length !== 0 && this.barChartsLabels != null) {
+  //     for (let i = 0; i <= this.barChartsLabels.length; i++) {
+  //       if (this.barChartsLabels[1].toLowerCase === cityName.toLowerCase) {
+  //         isFound = true;
+  //       }
+  //     }
+  //   }
 
-    return isFound;
-  }
+  //   return isFound;
+  // }
 
 
   showPie(){
@@ -109,41 +118,81 @@ export class ChartsComponent implements OnInit {
     var myChart = new Chart(ctx, {
     type: 'pie',
     data: {
-        // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        labels : this.PieChartsLabels,
-        datasets: [{
-            label: '# of Votes',
-            // data: [12, 19, 3, 5, 2, 3],
-            data: this.PieChartsData,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.9)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+      // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels : this.PieChartsLabels,
+      datasets: [{
+        label: '# of Votes',
+        // data: [12, 19, 3, 5, 2, 3],
+        data: this.PieChartsData,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.4)',
+          'rgba(54, 162, 235, 0.4)',
+          'rgba(255, 206, 86, 0.4)',
+          'rgba(75, 192, 192, 0.4)',
+          'rgba(153, 102, 255, 0.4)',
+          'rgba(255, 159, 64, 0.4)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
     },
     options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
     }
-});
+    });
+  }
+
+  showAppUsers(){
+    var ctx = document.getElementById("myPieChart");
+    var myPieChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: this.barChartsLabels,
+        datasets: [{
+          label: '# of Votes',
+          data: this.barChartsData,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero:true
+            }
+          }]
+        }
+      }
+    });
   }
 }
 
